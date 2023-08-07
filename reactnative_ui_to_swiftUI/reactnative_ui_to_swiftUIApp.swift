96e064d52b156f24bc5a254f5c200335515647b2
Crismcollins <cris.m.collins@gmail.com>
2023-08-07 16:40:55 -0400

    Initial Commit

---
+++ reactnative_ui_to_swiftUI/reactnative_ui_to_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  reactnative_ui_to_swiftUIApp.swift
+//  reactnative_ui_to_swiftUI
+//
+//  Created by Cristobal Molina Collins on 07-08-23.
+//
+
+import SwiftUI
+
+@main
+struct reactnative_ui_to_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
